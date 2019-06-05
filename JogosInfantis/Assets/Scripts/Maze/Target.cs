8c22e92478926da6c16fbd07ca55404f35b58c7f
Daybson B. S. P <daybson.paisante@outlook.com>
2019-06-05 14:58:12 -0300

    + cena não comitada

---
+++ JogosInfantis/Assets/Scripts/Maze/Target.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public Text text;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        text.gameObject.SetActive(true);
+        collision.transform.position = transform.position;
+        collision.GetComponent<BallFollower>().enabled = false;
+    }
+}
---
+++ JogosInfantis/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadScene(int i)
+    {
+        SceneManager.LoadScene(i);
+    }
+
+}
