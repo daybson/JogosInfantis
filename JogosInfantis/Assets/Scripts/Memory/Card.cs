ced333a545660b4d8c04b2f9292ed295be8bf1b9
Daybson Paisante <daybson.paisante@outlook.com>
2019-06-26 12:32:50 -0300

    + template de jogo da memória

---
+++ JogosInfantis/Assets/Scripts/Memory/Card.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    public int Id;
+    public bool Checked;
+    public Sprite draw;
+    public Sprite backface;
+    public SpriteRenderer spriteRenderer;
+    public Animator animator;
+    public float secondsToFlop;
+
+
+    private void Awake()
+    {
+        spriteRenderer.sprite = backface;
+    }
+
+
+    private void OnMouseDown()
+    {
+        if (CardChecker.Instance.Clicks >= 2)
+            return;
+
+        CardChecker.Instance.Clicks++;
+
+        Checked = true;
+        Flip();
+    }
+
+
+    public void Check()
+    {
+        CardChecker.Instance.SetPair(this);
+    }
+
+
+    public void Flip()
+    {
+        animator.SetBool("flip", true);
+    }
+
+
+    public void Flop()
+    {
+        animator.SetBool("flip", false);
+    }
+
+
+    public void SwitchSprite()
+    {
+        print("Switch");
+
+        if (Checked)
+            spriteRenderer.sprite = draw;
+        else
+            spriteRenderer.sprite = backface;
+    }
+}
---
+++ JogosInfantis/Assets/Scripts/Memory/CardChecker.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class CardChecker : Singleton<CardChecker>
+{
+    private List<Card> pair = new List<Card>();
+    public List<Card> remains = new List<Card>();
+
+    public int Clicks;
+
+
+    public void SetPair(Card card)
+    {
+        if (pair.Count < 2)
+            pair.Add(card);
+
+        if (pair.Count == 2)
+        {
+
+
+            if (pair[0].Id == pair[1].Id)
+                Match();
+            else
+                RemovePair();
+        }
+    }
+
+
+    public void RemovePair()
+    {
+        foreach (var c in pair)
+        {
+            c.Checked = false;
+            c.Flop();
+        }
+
+        Clicks = 0;
+
+        remains.ForEach(c => c.enabled = true);
+
+        remains.Clear();
+        pair.Clear();
+    }
+
+
+    public void Match()
+    {
+        foreach (var c in pair)
+        {
+            c.Checked = true;
+            c.animator.enabled = false;
+        }
+
+        Clicks = 0;
+        print("MATCH!!");
+    }
+}
