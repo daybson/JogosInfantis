4e400b82a471b1b672789b378788ab6b5894264e
Daybson Paisante <daybson.paisante@outlook.com>
2019-06-27 12:42:27 -0300

    + spawner de cards

---
+++ JogosInfantis/Assets/Scripts/Memory/CardSpanwer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class CardSpanwer : MonoBehaviour
+{
+    public List<GameObject> cards;
+    
+    //220 x 326
+    //1,48
+
+    public int totalCards;
+
+    private void Awake()
+    {
+        List<GameObject> clone = new List<GameObject>();
+
+        for (var i = 0; i < totalCards; i++)
+        {
+            var indexCard = Random.Range(0, cards.Count);
+
+            clone.Add(Instantiate(cards[indexCard]));
+            clone.Add(Instantiate(cards[indexCard]));
+        }
+    }
+
+
+    
+}
